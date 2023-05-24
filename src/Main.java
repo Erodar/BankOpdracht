ad4073e182b2187977d6bef8e852431a06df270b
Erodar <stijnrelou@gmail.com>
2023-05-25 00:01:58 +0200

    v1

--- src/Main.java
+++ src/Main.java
@@ -21,6 +21,7 @@ public class Main {
 //         Geld opnemen
 
         bank.withdraw("NL02EFGH1234567890", 200.0);
+        bank.deposit("NL01ABCD1234567890", 250.5);
 
 //         Saldo controleren
 
--- src/TransferHistory.java
+++ src/TransferHistory.java
@@ -7,26 +7,24 @@ import java.util.Objects;
 public class TransferHistory {
     public static HashMap<String, ArrayList<ArrayList<String>>> AllTransfers = new HashMap<>();
     public static ArrayList<ArrayList<String>> Transfers = new ArrayList<>();
-    public static ArrayList<ArrayList<String>> Temp = new ArrayList<>();
+
 
     public static void transfer_save(String AccountNumber, Double Deposit, String TransactionType) {
         ArrayList<String> Transfer = new ArrayList<>();
         boolean Found = false;
-        Transfers.clear();
         for (Map.Entry<String, ArrayList<ArrayList<String>>> set : AllTransfers.entrySet()) {
             if (Objects.equals(set.getKey(), AccountNumber)) {
-                Temp = new ArrayList<>(set.getValue());
+                Transfers = new ArrayList<>(set.getValue());
                 Found = true;
             }
         }
         if (!Found) {
-            Temp = new ArrayList<>();
+            Transfers = new ArrayList<>();
         }
         Transfer.add(TransactionType);
         Transfer.add(Deposit.toString());
         Transfers.add(Transfer);
-        Temp.addAll(Transfers);
-        AllTransfers.put(AccountNumber, Temp);
+        AllTransfers.put(AccountNumber, Transfers);
     }
 
     public static ArrayList<ArrayList<String>> display_history(String AccountNumber) {
