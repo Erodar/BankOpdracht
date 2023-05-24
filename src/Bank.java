a5f6ead94c2e5d15c49661ba08a16a55799297a5
Erodar <stijnrelou@gmail.com>
2023-05-25 00:19:44 +0200

    v1

--- src/Bank.java
+++ src/Bank.java
@@ -1,7 +1,10 @@
 import java.util.HashMap;
 
 public class Bank {
+    //create a hashmap that stores the account number and the attached BankAccount object
     public HashMap<String, BankAccount> map = new HashMap<>();
+
+    // Define value of currencies in a hashmap for later use in the currency exchange method
     public static HashMap<String, Double> currency;
     static {
         currency = new HashMap<>();
@@ -11,13 +14,14 @@ public class Bank {
         currency.put("Won", 1422.60);
     }
 
-
-    public void Create_Account(String Account_Number, Double First_Saldo) {
-        BankAccount Account = new BankAccount(Account_Number, First_Saldo);
+    //create a new account as a BankAccount object, and send the initial deposit to transfer history
+    public void Create_Account(String Account_Number, Double Initial_Deposit) {
+        BankAccount Account = new BankAccount(Account_Number, Initial_Deposit);
         map.put(Account_Number, Account);
-        TransferHistory.transfer_save(Account_Number, First_Saldo, "Initial Deposit");
+        TransferHistory.transfer_save(Account_Number, Initial_Deposit, "Initial Deposit");
     }
 
+    //returns the BankAccount object attached to an account number. If it doesn't exist, crashes.
     public BankAccount getAccount(String Account_Number) {
         try {
             return map.get(Account_Number);
@@ -26,19 +30,19 @@ public class Bank {
         }
     }
 
-
+    //uses the getaccount function to access the deposit method of the BankAccount class.
     public void deposit(String Account_Number, Double Deposit) {
         this.getAccount(Account_Number).deposit(Deposit);
         TransferHistory.transfer_save(Account_Number, Deposit, "Deposit");
     }
 
 
-
+    //same as previous, except now it's the withdraw method.
     public void withdraw(String Account_Number, Double Withdrawal) {
         this.getAccount(Account_Number).withdraw(Withdrawal);
         TransferHistory.transfer_save(Account_Number, Withdrawal, "Withdrawal");
     }
-
+    //for transfers. basically a combination of the last two methods in one.
     public void transfer(String Withdrawal_Account, String Deposit_Account, Double Transfer_Amount) {
         this.getAccount(Withdrawal_Account).withdraw(Transfer_Amount);
         TransferHistory.transfer_save(Withdrawal_Account, Transfer_Amount, "Withdrawal (Transfer)");
@@ -46,6 +50,7 @@ public class Bank {
         TransferHistory.transfer_save(Deposit_Account, Transfer_Amount, "Deposit (Transfer)");
     }
 
+    //recalculates the amount of money in the account based on the exchange rates defined at the top.
     public Double currency_exchange(String Account_Number, String Currency_Type) {
         return (this.getAccount(Account_Number).getBalance() * currency.get(Currency_Type));
     }
--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,18 +1,24 @@
 public class BankAccount {
+    //defining the two main attributes. AccountNumber is private and final since it never needs to be changed.
+    //the balance can be edited in other classes too, though, since there's no reason to make it private.
     private final String AccountNumber;
     public Double Balance;
 
+    //BankAccounts only have two attributes, the balance in the account and the account number.
     public BankAccount(String Account_Number, Double balance){
         this.AccountNumber = Account_Number;
         this.Balance = balance;
     }
+    //commented out because not used; just returns the accountnumber of an object.
 //    public String getAccountNumber() {
 //        return AccountNumber;
 //    }
+    //returns the balance of an object.
     public Double getBalance() {
         return Balance;
     }
 
+    //withdraws money from an account. if there isn't enough money it errors.
     public void withdraw (Double Withdraw_Amount) {
         if (this.Balance < Withdraw_Amount) {
             throw new RuntimeException("You don't have enough money in the account.");
@@ -20,12 +26,15 @@ public class BankAccount {
         this.Balance -= Withdraw_Amount;
     }
 
+    //deposits money into an account. If the amount is negative it errors.
     public void deposit (Double Deposit_Amount) {
         if (Deposit_Amount < 0) {
             throw new RuntimeException("You can't deposit a negative amount!");
         }
         this.Balance += Deposit_Amount;
     }
+
+    //i just used the constructor for a simple function that returns a bankaccount object as a string.
     @Override
     public String toString() {
         return "BankAccount{" +
